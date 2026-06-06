ec2963ab26007ca6fd4d992bdb1b82fbd8ca59e5
X5YD <github@akbaridev.ir>
2026-06-06 05:49:35 +0330

    Add prac.py

---
+++ prac.py
@@ -0,0 +1,22 @@
+#number = 1000000000.227
+#print(f"The Number:{number : =+20,.3f} ")
+#def intrest_calc(p, r, t):
+#    #p = starting money, r = intrest rate t = time
+#    A = p * (1 + r/100) ** t
+#    return A
+
+
+#def intrest_calc_ui():
+#    while True:
+#        try:
+#            user_p = input("Enter Your Princible [Type Quit To Stop] ")
+#            if user_p.lower() == "quit":
+#                break
+#            p = float(user_p)
+#            r = float(input("Whats The Intrest Rate? "))
+#            t = float(input("For How Many Years? "))
+#            result = intrest_calc(p, r, t)
+#            print(result)
+#        except ValueError:
+#            print("Thats Not A Number...! ")
+#intrest_calc_ui()
